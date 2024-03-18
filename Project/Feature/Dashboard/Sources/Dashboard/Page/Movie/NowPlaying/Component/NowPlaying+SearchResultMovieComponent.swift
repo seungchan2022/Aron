4fb7dd276b670aa89ca1e369836d987cf863953c
seungchan2022 <110214307+seungchan2022@users.noreply.github.com>
2024-03-18 23:10:16 +0900

    feat: 탭 액션 추가

--- Project/Feature/Dashboard/Sources/Dashboard/Page/Movie/NowPlaying/Component/NowPlaying+SearchResultMovieComponent.swift
+++ Project/Feature/Dashboard/Sources/Dashboard/Page/Movie/NowPlaying/Component/NowPlaying+SearchResultMovieComponent.swift
@@ -1,44 +1,48 @@
-import SwiftUI
-import DesignSystem
 import ComposableArchitecture
+import DesignSystem
+import SwiftUI
+
+// MARK: - NowPlayingPage.SearchResultMovieComponent
 
 extension NowPlayingPage {
   struct SearchResultMovieComponent {
     let viewState: ViewState
-    
+    let tapAction: () -> Void
+
     @Bindable var store: StoreOf<NowPlayingReducer>
     @Environment(\.colorScheme) var colorScheme
   }
 }
 
-extension NowPlayingPage.SearchResultMovieComponent {
+extension NowPlayingPage.SearchResultMovieComponent { }
 
-}
+// MARK: - NowPlayingPage.SearchResultMovieComponent + View
 
 extension NowPlayingPage.SearchResultMovieComponent: View {
-  
+
   @ViewBuilder
   var keyword: some View {
     LazyVStack(alignment: .leading) {
       Text("Keywords")
         .foregroundStyle(DesignSystemColor.palette(.gray(.lv300)).color)
         .padding(.top, 16)
-      
+
       if !viewState.keywordItemList.isEmpty {
         Divider()
       }
-      
+
       ForEach(viewState.keywordItemList, id: \.keyword) { item in
-        Button(action: {
-          print("keyword tap")
-        }) {
+        Button(action: { tapAction() }) {
           HStack {
             Text(item.keyword)
               .font(.system(size: 16))
-              .foregroundStyle(colorScheme == .dark ? DesignSystemColor.system(.white).color : DesignSystemColor.system(.black).color)
-            
+              .foregroundStyle(
+                colorScheme == .dark
+                  ? DesignSystemColor.system(.white).color
+                  : DesignSystemColor.system(.black).color)
+
             Spacer()
-            
+
             Image(systemName: "chevron.right")
               .resizable()
               .frame(width: 8, height: 12)
@@ -52,17 +56,17 @@ extension NowPlayingPage.SearchResultMovieComponent: View {
     .frame(maxWidth: .infinity, alignment: .leading)
     .padding(.leading, 16)
   }
-  
+
   @ViewBuilder
   var movie: some View {
     LazyVStack(alignment: .leading, spacing: 16) {
       Text("Result for \(store.query)")
         .foregroundStyle(DesignSystemColor.palette(.gray(.lv300)).color)
         .padding(.top, 16)
-      
+
       Divider()
         .padding(.top, -8)
-      
+
       if viewState.movieItemList.isEmpty {
         Text("No Results")
           .font(.system(size: 18))
@@ -70,11 +74,11 @@ extension NowPlayingPage.SearchResultMovieComponent: View {
           .padding(.top, -8)
 
         Divider()
-        
+
         Divider()
           .padding(.top, 24)
       }
-      
+
       ForEach(viewState.movieItemList) { item in
         Button(action: { }) {
           VStack {
@@ -83,22 +87,28 @@ extension NowPlayingPage.SearchResultMovieComponent: View {
                 .fill(.gray)
                 .frame(width: 100, height: 160)
                 .clipShape(RoundedRectangle(cornerRadius: 10))
-              
+
               VStack(alignment: .leading, spacing: 16) {
                 Text(item.title)
                   .font(.system(size: 18))
                   .foregroundStyle(DesignSystemColor.label(.ocher).color)
-                
+
                 HStack {
                   Text("\(Int(item.voteAverage * 10))%")
                     .font(.system(size: 18))
-                    .foregroundStyle(colorScheme == .dark ? DesignSystemColor.system(.white).color : DesignSystemColor.system(.black).color)
-                  
+                    .foregroundStyle(
+                      colorScheme == .dark
+                        ? DesignSystemColor.system(.white).color
+                        : DesignSystemColor.system(.black).color)
+
                   Text(item.releaseDate)
                     .font(.system(size: 16))
-                    .foregroundStyle(colorScheme == .dark ? DesignSystemColor.system(.white).color : DesignSystemColor.system(.black).color)
+                    .foregroundStyle(
+                      colorScheme == .dark
+                        ? DesignSystemColor.system(.white).color
+                        : DesignSystemColor.system(.black).color)
                 }
-                
+
                 if let overView = item.overView {
                   Text(overView)
                     .font(.system(size: 18))
@@ -107,9 +117,9 @@ extension NowPlayingPage.SearchResultMovieComponent: View {
                     .lineLimit(3)
                 }
               }
-              
+
               Spacer()
-              
+
               Image(systemName: "chevron.right")
                 .resizable()
                 .frame(width: 8, height: 12)
@@ -117,7 +127,7 @@ extension NowPlayingPage.SearchResultMovieComponent: View {
                 .padding(.trailing, 16)
             }
             .frame(maxWidth: .infinity, alignment: .leading)
-            
+
             Divider()
               .padding(.leading, 120)
           }
@@ -126,7 +136,7 @@ extension NowPlayingPage.SearchResultMovieComponent: View {
     }
     .padding(.leading, 16)
   }
-  
+
   var body: some View {
     ScrollView {
       VStack {
@@ -142,11 +152,11 @@ extension NowPlayingPage.SearchResultMovieComponent {
     let keywordItemList: [KeywordItem]
     let movieItemList: [MovieItem]
   }
-  
+
   struct KeywordItem: Equatable {
     let keyword: String
   }
-  
+
   struct MovieItem: Equatable, Identifiable {
     let id: Int
     let title: String
