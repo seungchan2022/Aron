2e9a80680622deed9bb94d555023e4204cc50add
승찬 <anstmdcks12@naver.com>
2024-04-14 17:50:53 +0900

    refactor: 리듀서 로직 사이드 이펙트로 이동

--- Modules/Feature/Dashboard/Sources/Dashboard/Page/MyList/MyListSideEffect.swift
+++ Modules/Feature/Dashboard/Sources/Dashboard/Page/MyList/MyListSideEffect.swift
@@ -32,7 +32,30 @@ extension MyListSideEffect {
       }
     }
   }
-
+  
+  var sortedByReleaseDate: ([MovieEntity.MovieDetail.MovieCard.Response]) -> [MovieEntity.MovieDetail.MovieCard.Response] {
+    { itemList in
+//      SortItem(
+//        itemList: itemList,
+//        process: itemList.releaseDate
+//      )
+      //        .sort()
+      (try? itemList.sorted(by: itemList.releaseDate)) ?? itemList
+    }
+  }
+  
+  var sortedByRating: ([MovieEntity.MovieDetail.MovieCard.Response]) -> [MovieEntity.MovieDetail.MovieCard.Response] {
+    { itemList in
+      (try? itemList.sorted(by: itemList.rating)) ?? itemList
+    }
+  }
+  
+  var sortedByPopularity: ([MovieEntity.MovieDetail.MovieCard.Response]) -> [MovieEntity.MovieDetail.MovieCard.Response] {
+    { itemList in
+      (try? itemList.sorted(by: itemList.rating)) ?? itemList
+    }
+  }
+  
   var routeToNewList: () -> Void {
     {
       navigator.sheet(
@@ -55,5 +78,44 @@ extension MyListSideEffect {
 extension MovieEntity.MovieDetail.MovieCard.Response {
   fileprivate func serialized() -> MovieEntity.MovieDetail.MovieCard.Request {
     .init(movieID: id)
+  }
+}
+
+extension MyListSideEffect {
+  fileprivate struct SortItem<T: Equatable> {
+    let itemList: [T]
+    let process: (T, T) throws -> Bool
+    
+    func sort() -> [T] {
+      (try? itemList.sorted(by: process)) ?? itemList
+    }
+  }
+}
+
+extension [MovieEntity.MovieDetail.MovieCard.Response] {
+  fileprivate  var releaseDate: (MovieEntity.MovieDetail.MovieCard.Response, MovieEntity.MovieDetail.MovieCard.Response) throws -> Bool {
+    {
+      $0.releaseDate.toDate ?? Date() > $1.releaseDate.toDate ?? Date()
+    }
+  }
+  
+  fileprivate var rating: (MovieEntity.MovieDetail.MovieCard.Response, MovieEntity.MovieDetail.MovieCard.Response) throws -> Bool {
+    {
+      $0.voteAverage ?? 0 > $1.voteAverage ?? 0
+    }
+  }
+  
+  fileprivate var popularity: (MovieEntity.MovieDetail.MovieCard.Response, MovieEntity.MovieDetail.MovieCard.Response) throws -> Bool {
+    {
+      $0.popularity > $1.popularity
+    }
+  }
+}
+
+extension String {
+  fileprivate var toDate: Date? {
+    let dateFormatter = DateFormatter()
+    dateFormatter.dateFormat = "yyyy-MM-dd"
+    return dateFormatter.date(from: self)
   }
 }
